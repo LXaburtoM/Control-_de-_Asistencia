fb5522b4f7cc68e5499a4273eccc239bdec37c2d
FICT1 <isaquitopro10@gmail.com>
2026-03-20 15:26:46 -0600

    Instrucciones y psvm agregado

---
+++ ControldeAsistencias/src/Main/Main.java
@@ -0,0 +1,11 @@
+package Main;
+//Se desarrollará un sistema para controlar la asistencia de estudiantes mediante clases.
+//El sistema recorrerá una lista de estudiantes usando estructuras repetitivas.
+//Se contabilizarán presentes y ausentes durante el proceso.
+//Finalmente, se mostrará un resumen de la asistencia general.
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
