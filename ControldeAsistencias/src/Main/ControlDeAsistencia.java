6f62ca2195ac72bf061823b5959aba897f9bff2e
vybdani <darochap@uamv.edu.ni>
2026-03-20 15:52:22 -0600

    Enunciado

---
+++ ControldeAsistencias/src/Main/ControlDeAsistencia.java
@@ -0,0 +1,12 @@
+package Main;
+
+//Se desarrollará un sistema para controlar la asistencia de estudiantes mediante clases.
+//El sistema recorrerá una lista de estudiantes usando estructuras repetitivas.
+//Se contabilizarán presentes y ausentes durante el proceso.
+//Finalmente, se mostrará un resumen de la asistencia general.
+
+public class ControlDeAsistencia {
+ public static void main(String[] args) {
+
+ }
+}
